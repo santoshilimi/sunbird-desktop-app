bd2384288d738068caac1e905b1d6c8b0387d773
Harishkumar Gangula <harishg@ilimi.in>
2019-05-20 18:55:41 +0530

    Issue #SB-10822 feat:  added portal as dependency

---
+++ src/gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require("gulp");
+const download = require("download-git-repo");
+const exec = require("child_process").exec;
+const fs = require('fs-extra')
+
+
+gulp.task("download:portal", (cb) => {
+    download("Sunbird-Ed/SunbirdEd-portal#release-2.0.0", "temp/portal", function (err) {
+        cb(err)
+    })
+})
+
+gulp.task("client:install", (cb) => {
+    exec("npm install  --prefix ./temp/portal/src/app/client", { maxBuffer: Infinity }, function (err, stdout, stderr) {
+        console.log(stdout)
+        console.log(stderr)
+        cb(err)
+    })
+});
+
+gulp.task("offline-client:dist", (cb) => {
+    exec("npm run build-offline-prod --prefix ./temp/portal/src/app/client ", { maxBuffer: Infinity }, function (err, stdout, stderr) {
+        console.log(stdout)
+        console.log(stderr)
+        cb(err)
+    })
+});
+
+gulp.task("copy:plugins", (cb) => {
+    fs.move("./temp/portal/src/app/sunbird-plugins/", "./public/sunbird-plugins/", { overwrite: true }, (err) => {
+        cb(err)
+    })
+})
+
+gulp.task("copy:portal", (cb) => {
+    fs.move("./temp/portal/src/app/dist/", "./public/portal/", { overwrite: true }, (err) => {
+        cb(err)
+    })
+})
+
+gulp.task("default", gulp.series("download:portal",
+    "client:install",
+    "offline-client:dist",
+    "copy:portal",
+    "copy:plugins"));
