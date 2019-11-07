596dbd327ac3802d3a118f07db7735ed7e7f208f
Harishkumar Gangula <harishg@ilimi.in>
2019-11-07 10:59:58 +0530

    Issue #SB-15083 feat: added instrumentation for splash screen

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,11 @@ import { frameworkConfig } from "./framework.config";
 import express from "express";
 import portscanner from "portscanner";
 import * as bodyParser from "body-parser";
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
+import { Subject } from "rxjs";
+import { debounceTime } from "rxjs/operators";
 import { HTTPService } from "@project-sunbird/ext-framework-server/services";
 import * as os from "os";
-const { URL } = require("url");
-const uuid = require("uuid/v4");
+const startTime = Date.now();
 let envs = {};
 const windowIcon = path.join(__dirname, "build", "icons", "png", "512x512.png");
 // Keep a global reference of the window object, if you don't, the window will
@@ -29,47 +28,53 @@ let isAppBootstrapped = false;
 
 const expressApp = express();
 expressApp.use(bodyParser.json());
-let fileSDK = containerAPI.getFileSDKInstance('');
+let fileSDK = containerAPI.getFileSDKInstance("");
 
-const reloadUiOnFileChange = () => {
+const reloadUIOnFileChange = () => {
   const subject = new Subject<any>();
   subject.pipe(debounceTime(2500)).subscribe(data => {
     let currentURL = win.webContents.getURL();
-    console.log('portal file changed- reloading screen with current url', currentURL);
-    fs.rename(path.join('public', 'portal','index.html'), path.join('public', 'portal','index.ejs'), (err) => {
-      if ( err ) console.log('ERROR: ' + err);
-        win.loadURL('http://localhost:9000/' || currentURL);
-    });
+    console.log(
+      "portal file changed- reloading screen with current url",
+      currentURL
+    );
+    fs.rename(
+      path.join("public", "portal", "index.html"),
+      path.join("public", "portal", "index.ejs"),
+      err => {
+        if (err) console.log("ERROR: " + err);
+        win.reload();
+      }
+    );
   });
-  fileSDK.watch([path.join('public', 'portal')])
-  .on('add', path => subject.next(path))
-  .on('change', path => subject.next(path))
-  .on('unlink', path => subject.next(path));
-}
+  fileSDK
+    .watch([path.join("public", "portal")])
+    .on("add", path => subject.next(path))
+    .on("change", path => subject.next(path))
+    .on("unlink", path => subject.next(path));
+};
 if (!app.isPackaged) {
-  reloadUiOnFileChange();
+  reloadUIOnFileChange();
 }
-expressApp.use('/dialog/content/import', (req, res) => {
-  res.send({ message: 'SUCCUSS', responseCode: 'OK'});
+expressApp.use("/dialog/content/import", (req, res) => {
+  res.send({ message: "SUCCESS", responseCode: "OK" });
   importContent();
 });
 
 const importContent = () => {
-  const path = dialog.showOpenDialog({ 
-    properties: ['openFile', 'multiSelections'], 
-    filters: [{ name: 'Custom File Type', extensions: ['ecar'] }] 
+  const path = dialog.showOpenDialog({
+    properties: ["openFile", "multiSelections"],
+    filters: [{ name: "Custom File Type", extensions: ["ecar"] }]
   });
-  if(path){
+  if (path) {
     makeImportApiCall(path);
   }
-}
+};
 
 const getFilesPath = () => {
-  if (_.startsWith(_.toLower(envs["APP_ID"]), "local")) {
-    return __dirname;
-  } else {
-    return path.join(app.getPath("userData"), "." + envs["APP_NAME"]);
-  }
+  return app.isPackaged
+    ? path.join(app.getPath("userData"), "." + envs["APP_NAME"])
+    : __dirname;
 };
 
 // set the env
@@ -77,24 +82,47 @@ const initializeEnv = () => {
   envs = JSON.parse(
     fs.readFileSync(path.join(__dirname, "env.json"), { encoding: "utf-8" })
   );
+  let rootOrgObj = JSON.parse(
+    fs.readFileSync(
+      path.join(
+        __dirname,
+        frameworkConfig.plugins[0].id,
+        "data",
+        "organizations",
+        `${envs["CHANNEL"]}.json`
+      ),
+      { encoding: "utf-8" }
+    )
+  );
+  process.env.ROOT_ORG_ID =
+    _.get(rootOrgObj, "result.response.content[0].rootOrgId") ||
+    _.get(rootOrgObj, "result.response.content[0].hashTagId");
+  process.env.ROOT_ORG_HASH_TAG_ID = _.get(
+    rootOrgObj,
+    "result.response.content[0].hashTagId"
+  );
+  process.env.TELEMETRY_VALIDATION = app.isPackaged ? "false" : "true";
+  process.env.APP_VERSION = app.getVersion();
+
   _.forEach(envs, (value, key) => {
     process.env[key] = value;
   });
   process.env.DATABASE_PATH = path.join(getFilesPath(), "database");
   process.env.FILES_PATH = getFilesPath();
-  console.log(`FILES_PATH for the app is ${process.env.FILES_PATH}`);
   if (!fs.existsSync(process.env.DATABASE_PATH)) {
     fse.ensureDirSync(process.env.DATABASE_PATH);
   }
 };
 
 const copyPluginsMetaData = async () => {
-  if (!_.startsWith(_.toLower(envs["APP_ID"]), "local")) {
+  if (app.isPackaged) {
     for (const plugin of frameworkConfig.plugins) {
-      await fse.copy(
-        path.join(__dirname, plugin.id),
-        path.join(getFilesPath(), plugin.id)
-      );
+      if (!fs.existsSync(path.join(getFilesPath(), plugin.id))) {
+        await fse.copy(
+          path.join(__dirname, plugin.id),
+          path.join(getFilesPath(), plugin.id)
+        );
+      }
     }
   }
 };
@@ -119,6 +147,7 @@ const framework = async () => {
         resolve();
       })
       .catch((error: any) => {
+        console.error(error);
         resolve();
       });
   });
@@ -151,7 +180,6 @@ const checkPluginsInitialized = () => {
 };
 // start loading all the dependencies
 const bootstrapDependencies = async () => {
-  initializeEnv();
   await copyPluginsMetaData();
   await setAvailablePort();
   await framework();
@@ -165,6 +193,8 @@ const bootstrapDependencies = async () => {
 };
 
 function createWindow() {
+  //initialize the environment variables
+  initializeEnv();
   //splash screen
   let splash = new BrowserWindow({
     width: 300,
@@ -175,50 +205,65 @@ function createWindow() {
     icon: windowIcon
   });
 
+  splash.once("show", () => {
+    let telemetryInstance = containerAPI
+      .getTelemetrySDKInstance()
+      .getInstance();
+    telemetryInstance.impression({
+      context: {
+        env: "home"
+      },
+      edata: {
+        type: "view",
+        pageid: "splash",
+        uri: "loading/index.html",
+        duration: (Date.now() - startTime) / 1000
+      }
+    });
 
-  splash.once('show', () => {
-      // Create the main window.
-      win = new BrowserWindow({
-        titleBarStyle: "hidden",
-        show: false,
-        minWidth: 700,
-        minHeight: 500,
-        webPreferences: {
-          nodeIntegration: false
-        },
-        icon: windowIcon
+    // Create the main window.
+    win = new BrowserWindow({
+      titleBarStyle: "hidden",
+      show: false,
+      minWidth: 700,
+      minHeight: 500,
+      webPreferences: {
+        nodeIntegration: false
+      },
+      icon: windowIcon
+    });
+
+    win.webContents.once("dom-ready", () => {
+      splash.destroy();
+      win.show();
+      win.maximize();
+    });
+
+    // create admin for the database
+    bootstrapDependencies()
+      .then(() => {
+        appBaseUrl = `http://localhost:${process.env.APPLICATION_PORT}`;
+        win.loadURL(appBaseUrl);
+        win.focus();
+        // Open the DevTools.
+        // win.webContents.openDevTools();
+
+        isAppBootstrapped = true;
+        checkForOpenFileInWindows();
+        if (openFileContents.length > 0) {
+          makeImportApiCall(openFileContents);
+        }
+      })
+      .catch(err => {
+        logger.error("unable to start the app ", err);
       });
-
-      win.webContents.once('dom-ready', () => {
-        splash.destroy();
-        win.show();
-        win.maximize();
-        });
-
-        // create admin for the database
-        bootstrapDependencies()
-        .then(() => {
-            appBaseUrl = `http://localhost:${process.env.APPLICATION_PORT}`;
-            win.loadURL(appBaseUrl);
-            win.focus();
-            // Open the DevTools.
-            // win.webContents.openDevTools();
-            isAppBootstrapped = true;
-            checkForOpenFileInWindows();
-            if (openFileContents.length > 0) {
-              makeImportApiCall(openFileContents);
-            }
-        })
-        .catch(err => {
-          logger.error("unable to start the app ", err);
-        });
-          // Emitted when the window is closed.
-          win.on("closed", () => {
-            // Dereference the window object, usually you would store windows
-            // in an array if your app supports multi windows, this is the time
-            // when you should delete the corresponding element.
-            win = null;
-          });
+    // Emitted when the window is closed.
+    win.on("closed", () => {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      win = null;
+    });
   });
   splash.loadFile(path.join(__dirname, "loading", "index.html"));
   splash.show();
@@ -286,9 +331,17 @@ app.on("open-file", (e, path) => {
 });
 
 const makeImportApiCall = async (contents: Array<string>) => {
-  await HTTPService.post(`${appBaseUrl}/api/content/v1/import`, contents).toPromise()
-  .then(data => logger.info('Content import started successfully', contents))
-  .catch(error => logger.error('Content import failed with', error, 'for contents', contents));
+  await HTTPService.post(`${appBaseUrl}/api/content/v1/import`, contents)
+    .toPromise()
+    .then(data => logger.info("Content import started successfully", contents))
+    .catch(error =>
+      logger.error(
+        "Content import failed with",
+        error,
+        "for contents",
+        contents
+      )
+    );
 };
 
 // to handle ecar file open in windows
@@ -308,5 +361,3 @@ const checkForOpenFileInWindows = (files?: string[]) => {
     );
   }
 };
-
-
