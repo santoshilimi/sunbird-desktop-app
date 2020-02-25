2016371127232d874bab277c139a9209534d532e
Harishkumar Gangula <harishg@ilimi.in>
2020-02-25 11:17:14 +0530

    Issue #SB-17717 fix:  updated portal branch

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 gulp.task("download:portal", cb => {
   download(
-    "Sunbird-Ed/SunbirdEd-portal#release-2.7.0",
+    "Sunbird-Ed/SunbirdEd-portal#release-2.8.0",
     "temp/portal",
     cb
   );
