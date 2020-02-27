788b60e77e1b574b308d466ad5e895f7a1000f97
Harishkumar Gangula <harishg@ilimi.in>
2020-02-27 18:11:44 +0530

    Issue #SB-000 fix: Updated the build steps

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd src
 npm install
 npm run build
 cd temp/portal/src/app/client
-node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --project desktop --prod && mv ../dist/index.html ../dist/index.ejs
+node --max_old_space_size=4096 ./node_modules/@angular/cli/bin/ng build --project desktop --prod && mv ../dist/index.html ../dist/index.ejs
 cd ../../../../../
 npm run build-copy-clean
 cd ..
