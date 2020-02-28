50243885ca80f450b08d7582ae701ef407dad198
Harishkumar Gangula <harishg@ilimi.in>
2020-02-28 11:55:09 +0530

    Issue #SB-000 fix: Updated the build step

--- build.sh
+++ build.sh
@@ -6,8 +6,10 @@ npm install
 npm run build
 export LIMIT=12096
 npm install -g increase-memory-limit
+cd /temp/portal/src/app/client
 increase-memory-limit
-npm run offline-prod --prefix ./temp/portal/src/app/client
+npm run offline-prod 
+cd ../../../../../
 npm run build-copy-clean
 cd ..
 tar -czvf src.tar.gz src
